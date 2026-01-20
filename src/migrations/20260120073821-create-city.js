c366c2218f7ea195f4d78559605d4e0fbe6c9a05
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-01-20 13:14:47 +0530

    Added city model and migrations

---
+++ src/migrations/20260120073821-create-city.js
@@ -0,0 +1,30 @@
+"use strict";
+//this file contain constraint on the database level
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Cities", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false, //on database level constrain
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Cities");
+  },
+};
